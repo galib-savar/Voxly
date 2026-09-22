cb9d06115bebedd108213954c4c4c69a79089fe5
Galib <galib.savar@gmail.com>
2026-09-22 18:56:06 +0600

    Update CORS middleware to support additional HTTP methods

--- backend/src/middlewares/cors.middleware.js
+++ backend/src/middlewares/cors.middleware.js
@@ -15,7 +15,7 @@ const corsMiddleware = cors({
     callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 });
 
